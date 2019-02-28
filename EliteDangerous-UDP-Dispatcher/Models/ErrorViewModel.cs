5e8ad40515cab07634b4924e8fcf4b827e7402ba
Chris Gårdenberg <webmaster@itssimple.se>
2019-02-28 18:45:25 +0100

    Add project files.

---
+++ EliteDangerous-UDP-Dispatcher/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EliteDangerous_UDP_Dispatcher.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
