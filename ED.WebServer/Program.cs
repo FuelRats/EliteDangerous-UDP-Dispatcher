b72b4197cd138833f7848ff18c4b942d8433c29d
Chris Gårdenberg <webmaster@itssimple.se>
2019-03-05 20:39:33 +0100

    Moved UdpListener and Dispatchers to their own projects. And renamed project to ED.WebServer

---
+++ ED.WebServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace EliteDangerous_UDP_Dispatcher
+{
+	public class Program
+	{
+		static readonly CancellationTokenSource cts = new CancellationTokenSource();
+
+		public async static Task Main(string[] args)
+		{
+			Console.CancelKeyPress += SelfDestructAllTheThings;
+			var webHost = RunWebHost(cts.Token, args);
+
+			Task.WaitAll(webHost);
+			await Task.CompletedTask;
+		}
+
+		static void SelfDestructAllTheThings(object sender, ConsoleCancelEventArgs e)
+		{
+			Console.CancelKeyPress -= SelfDestructAllTheThings;
+			cts.Cancel();
+		}
+
+		async static Task RunWebHost(CancellationToken token, string[] args) =>
+			await WebHost.CreateDefaultBuilder(args)
+			.UseShutdownTimeout(TimeSpan.FromSeconds(30))
+				.UseStartup<Startup>()
+			.Build()
+			.RunAsync(token);
+	}
+}
